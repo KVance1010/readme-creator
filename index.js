3f0cd78eb0e24e2fce89a019472ebdf8fc9fc068
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-10-18 21:40:07 -0700

    added an array of objects

--- index.js
+++ index.js
@@ -1,34 +1,87 @@
 // Include packages needed for this application
-const inquirerFunction = require ('inquirer');
-const fileSystem = require ('fs');
-const path = require('path');
+const inquirerFunction = require('inquirer');
+const fileSystem = require('fs');
+const filePath = require('path');
+const markdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
-"What is the name of the project?",
-"Give an overview of the project?",
-"What Technologies were used?",
-" "
+	// 'What is the name of the project?',
+	// 'Give an overview of the project?',
+	// 'Learning Objectives of the project?',
+	// 'What Technologies were used?',
+	// 'What were the user stories?',
+	// 'What were the acceptance criteria?',
+	// 'What license was used?',
+	// 'Link to live source?',
+	{
+		name: 'question1',
+		message: 'What is the name of the project?',
+		type: 'input',
+	},
+	{
+		name: 'question2',
+		message: 'Give an overview of the project?',
+		type: 'input',
+	},
+	{
+		name: 'question3',
+		message: 'Learning Objectives of the project?',
+		type: 'input',
+	},
+	{
+		name: 'question4',
+		message: 'What Technologies were used?',
+		type: 'input',
+	},
+	{
+		name: 'question5',
+		message: 'What were the user stories?',
+		type: 'input',
+	},
+	{
+		name: 'question6',
+		message: 'What were the acceptance criteria?',
+		type: 'input',
+	},
+	{
+		name: 'question7',
+		message: 'What license was used?',
+		type: 'input',
+	},
+	{
+		name: 'question8',
+		message: 'Link to live source?',
+		type: 'input',
+	},
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
-fileSystem.writeFile(path.join(__dirname,'randomRead.md'),'info to write to file' ,  (error)=> {console.log(error);});
+// Creates a function to write a README file
+function writeToFile(fileName, data) {
+	fileSystem.writeFile(filePath.join(__dirname, fileName), data, (error) => {
+		console.log(error);
+	});
 }
 
-// TODO: Create a function to initialize app
+// Initialize app
 function init() {
-//   questions.forEach(element => {
-    inquirerFunction.prompt([{
-        name:"question1",
-        message:"message",
-        type:"input",
-        }]).then(function(data){
-            console.log(data);
-            writeToFile("random", "12jl");
-        })}
-// })
+	prompt(questions).then(function (data) {
+		console.log(data);
+	});
+	// inquirerFunction
+	// 	.prompt([{
+	// 			name: 'question' + i,
+	// 			message: 'question',
+	// 			type: 'input'
+	// 		}])
+	// 	.then(function (data) {
+	//         console.log(data);
+	// 	});
+}
 
+// })
 
 // Function call to initialize app
 init();
+
+
